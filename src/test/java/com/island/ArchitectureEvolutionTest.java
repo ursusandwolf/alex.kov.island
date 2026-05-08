82e2dd8a306229395038eb44bac3acf910dbfc9c
alex <ursusandwolf@protonmail.com>
2026-05-08 11:47:23 +0300

    refactor: remove redundant engine.event domain events

--- src/test/java/com/island/ArchitectureEvolutionTest.java
+++ src/test/java/com/island/ArchitectureEvolutionTest.java
@@ -3,49 +3,23 @@ package com.island;
 import com.island.engine.ecs.Component;
 import com.island.engine.ecs.ComponentRegistry;
 import com.island.engine.event.DefaultEventBus;
-import com.island.engine.event.EntityDiedEvent;
 import com.island.engine.event.EventBus;
 import com.island.nature.config.Configuration;
 import com.island.nature.entities.components.HealthComponent;
 import com.island.nature.entities.components.MovementComponent;
 import java.util.HashMap;
-import java.util.concurrent.atomic.AtomicReference;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import com.island.engine.model.Mortal;
 import com.island.nature.entities.core.Animal;
 import com.island.nature.entities.core.AnimalType;
 import com.island.nature.entities.core.Organism;
 import com.island.nature.entities.core.SpeciesKey;
 
 class ArchitectureEvolutionTest {
-
-    @Test
-    @DisplayName("EventBus should correctly deliver EntityDiedEvent to subscribers")
-    void eventBusShouldDeliverEvents() {
-        EventBus eventBus = new DefaultEventBus();
-        AtomicReference<EntityDiedEvent> caughtEvent = new AtomicReference<>();
-        
-        eventBus.subscribe(EntityDiedEvent.class, caughtEvent::set);
-        
-        // Mock a mortal entity
-        Mortal entity = new Mortal() {
-            @Override public boolean isAlive() { return true; }
-            @Override public void die() {}
-            @Override public String getTypeName() { return "Test"; }
-        };
-        EntityDiedEvent event = new EntityDiedEvent(entity, "TEST_CAUSE");
-        
-        eventBus.publish(event);
-        
-        assertNotNull(caughtEvent.get());
-        assertEquals("TEST_CAUSE", caughtEvent.get().getCause());
-        assertEquals(entity, caughtEvent.get().getEntity());
-    }
 
     @Test
     @DisplayName("Organism should support dynamic components (ECS)")
