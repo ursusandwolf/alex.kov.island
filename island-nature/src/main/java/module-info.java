ecb891fc1ecf454829c4565786a281f0cbee617a
alex <ursusandwolf@protonmail.com>
2026-05-11 19:59:50 +0300

    fix(app): fix JPMS module descriptors for ServiceLoader plugin discovery
    - Add module-info.java to island-app
    - Export necessary packages from island-nature and island-simcity
    - Add logback-classic to island-app for runtime logging

--- island-nature/src/main/java/module-info.java
+++ island-nature/src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.island.nature {
     exports com.island.nature.config;
     exports com.island.nature.view;
     exports com.island.nature.entities.core;
+    exports com.island.nature.model;
 
     provides com.island.engine.core.SimulationPlugin with com.island.nature.NaturePlugin;
 }
